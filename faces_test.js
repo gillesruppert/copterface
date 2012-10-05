6bceed8c2f305853c4a54ea1c941cf1d8f1199bc
Tõnis Tiigi <tonistiigi@gmail.com>
2012-10-05 13:46:57 +0200

    Splitting up to multiple files

---
+++ faces_test.js
@@ -0,0 +1,39 @@
+var arDrone = require('ar-drone');
+var path = require('path');
+var cv = require('opencv');
+var tako = require('tako');
+var app = tako();
+var fs = require('fs');
+
+
+var client  = arDrone.createClient();
+client.createRepl();
+
+
+var Faces = require('./faces')
+
+
+var f = new Faces();
+
+function logStatus() {
+  f.status(function(reply) {
+    console.log('reply', reply);
+    setTimeout(function(){
+      logStatus();
+    }, 100)
+  })
+}
+
+logStatus();
+
+app.route('/last.png').on('request', function(req, resp){
+  resp.write(f.lastImage_ || '');
+})
+app.route('/debug').on('request', function(req, resp){
+  fs.createReadStream('./static/debug.htm').pipe(resp);
+})
+app.route('*').on('request', function(req, resp){
+  resp.end();
+})
+
+app.httpServer.listen(3200)
